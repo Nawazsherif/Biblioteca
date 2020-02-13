92bf997e9cbf1b2a2fffa6fa5b96f90d7a6eaf6f
Nawazsherif <nawaz.sherifn1@thoughtworks.com>
2020-02-13 16:23:18 +0530

    [Nawaz] Fix. Movie objects

--- src/test/java/com/tw/university/MovieTest.java
+++ src/test/java/com/tw/university/MovieTest.java
@@ -1,23 +1,55 @@
 package com.tw.university;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MovieTest {
+
+    Movie movie1;
+    Movie movie2;
+
+    @BeforeEach
+    public void setUp()
+    {
+       movie1 = new Movie("Ninja storms", 1990, "Cameron", 8);
+       movie2 = new Movie("Ninja storms", 1990, "Cameron", 8);
+    }
+
     @Test
     public void shouldEquateMoviesWithSameDetails() {
-        Movie movie1 = new Movie("Ninja storms", 1990, "Cameron", 8);
-        Movie movie2 = new Movie("Ninja storms", 1990, "Cameron", 8);
+        movie1 = new Movie("Ninja storms", 1990, "Cameron", 8);
+        movie2 = new Movie("Ninja storms", 1990, "Cameron", 8);
 
         assertEquals(movie1, movie2);
     }
 
     @Test
     public void shouldEquateHashCodesOfMoviesWithSameDetails() {
-        Movie movie1 = new Movie("Ninja storms", 1990, "Cameron", 8);
-        Movie movie2 = new Movie("Ninja storms", 1990, "Cameron", 8);
+        movie1 = new Movie("Ninja storms", 1990, "Cameron", 8);
+        movie2 = new Movie("Ninja storms", 1990, "Cameron", 8);
 
         assertEquals(movie1.hashCode(), movie2.hashCode());
+    }
+
+    @Test
+    public void shouldReturnValidMovieName() {
+        assertEquals(movie1.name(),"Ninja storms");
+    }
+
+    @Test
+    public void shouldReturnValidMovieDirector() {
+        assertEquals(movie1.director(),"Cameron");
+    }
+
+    @Test
+    public void shouldReturnValidMovieYear() {
+        assertEquals(movie1.year(),1990);
+    }
+
+    @Test
+    public void shouldReturnValidMovieRating() {
+        assertEquals(movie1.rating(),8);
     }
 }
